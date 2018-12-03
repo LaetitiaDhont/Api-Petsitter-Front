00ed1a25a2ff3a3a49dfab6b8931feff645d64b3
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-12-03 22:45:06 +0100

    Header + HomePage (amélioration du front + informations)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,34 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { AnnoncesListComponent } from './annonces-list/annonces-list.component';
 import { ReservationsListComponent } from './reservations-list/reservations-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 const appRoutes: Routes = [
-  { path: 'reservations', component: ReservationsListComponent },
+  { path: '', component: HomepageComponent},
+  { path: 'header.component.html', component: HeaderComponent },
   { path: 'annonces', component: AnnoncesListComponent },
+  { path: 'reservations', component: ReservationsListComponent },
   { path: 'articles', component: ArticlesListComponent },
-  { path: '', component: ReservationsListComponent }
+  { path: 'footer.component.html', component: FooterComponent }
 
 ];
 
 @NgModule({
   declarations: [
+    HomepageComponent,
     AppComponent,
     AnnoncesListComponent,
     ReservationsListComponent,
-    ArticlesListComponent
+    ArticlesListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
