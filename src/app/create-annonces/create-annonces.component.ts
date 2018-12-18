28d8827343a427e360580eaf937234ef571a9b87
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-12-18 10:09:26 +0100

    Création du POST pour créer une annonce.

--- src/app/create-annonces/create-annonces.component.ts
+++ src/app/create-annonces/create-annonces.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Annonce } from '../model/annonces';
+import { AnnoncesService } from '../services/annonces.service';
 
 @Component({
   selector: 'app-create-annonces',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAnnoncesComponent implements OnInit {
 
-  constructor() { }
+  annonce: Annonce = new Annonce();
+  submitted = false;
+
+  constructor(private annonceService: AnnoncesService) { }
 
   ngOnInit() {
   }
+
+   newAnnonce(): void {
+     this.submitted = false;
+     this.annonce = new Annonce();
+   }
+
+   save() {
+     this.annonceService.createAnnonce(this.annonce)
+     .subscribe(data => console.log(data), error => console.log(error));
+     this.annonce = new Annonce();
+   }
+
+   onSubmit() {
+     this.submitted = true;
+     this.save();
+   }
 
 }
--- src/app/services/annonces.service.ts
+++ src/app/services/annonces.service.ts
@@ -8,10 +8,14 @@ import { Observable, from } from 'rxjs';
   providedIn: 'root'
 })
 export class AnnoncesService {
-
+  private baseUrl = 'http://localhost:8080/api/annonces';
   constructor(private httpclient: HttpClient) { }
 
   public getAnnonces(): Observable<Annonce[]> {
     return this.httpclient.get<Annonce[]>('http://localhost:8080/api/annonces');
    }
+
+   public createAnnonce(annonce: Object): Observable<Object> {
+    return this.httpclient.post(`${this.baseUrl}` + `/create`, annonce);
+  }
 }
